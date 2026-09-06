6faaae98b7446b83e838f61bf04b928c3ea0ffe6
breno <brenodonascimentosilvaa@gmail.com>
2026-09-06 19:07:46 -0300

    remove unnecessary output

--- outputs.tf
+++ outputs.tf
@@ -8,16 +8,6 @@ output "load_balancer_dns_name" {
   description = "DNS name of the Application Load Balancer"
 }
 
-output "certificate_validation_cname" {
-  description = "Create this CNAME at your DNS provider, then run terraform apply again"
-
-  value = length(aws_acm_certificate.cert) == 0 ? null : {
-    type      = one(aws_acm_certificate.cert[0].domain_validation_options).resource_record_type
-    name      = one(aws_acm_certificate.cert[0].domain_validation_options).resource_record_name
-    points_to = one(aws_acm_certificate.cert[0].domain_validation_options).resource_record_value
-  }
-}
-
 output "certificate_validation_tutorial" {
   description = "Human-readable instructions for validating the ACM certificate"
 

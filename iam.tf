0c127c2a4d19dff73b2c0a142d73c317e7b6f261
breno <brenodonascimentosilvaa@gmail.com>
2026-09-07 16:07:35 -0300

    add shell access without port 22

--- iam.tf
+++ iam.tf
@@ -35,6 +35,11 @@ resource "aws_iam_role_policy" "read_database_secret" {
 
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+  role       = aws_iam_role.wordpress.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "wordpress" {
   name = "wordpress-ec2"
   role = aws_iam_role.wordpress.name
